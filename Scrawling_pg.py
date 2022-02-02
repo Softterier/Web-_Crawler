58e1674903b139db4d96cf7d84b9ab09d02c089b
Softterier <43970002+Softterier@users.noreply.github.com>
2022-02-02 08:32:17 -0500

    Create_Scrawling_pG.py
    Pagina para Scrawling

---
+++ Scrawling_pg.py
@@ -0,0 +1,40 @@
+#Phyton File
+#Scrawler  a la Pagina Web AS Diario en España de Futbol
+Front AS4 import.BeatifulSoap
+#Importar Libreria Request descargar imformacion de una Pagina HTML WEB
+Import Request
+#Importar Libreria Panda para Clasificar filas y Columnas
+Import Panda AS PandaData
+#Importar Libreria BeatifulSoap para  
+import BeautifulSoup
+#URL Uniform Resource Locator 
+url = 
+#div#didomi-Host   Aceptar Cookies
+#didomi-host > div
+div id="didomi-notice" 
+class="didomi-regular-notice didomi-banner-notice-info-type didomi-notice-banner shape-banner top didomi-regular-notice-with-data-processing didomi-support-full-height" 
+style="background-color: rgb(255, 255, 255); color: rgb(0, 0, 0); border-color: rgb(1, 108, 162);">
+<div class="didomi-notice__interior-border didomi-border" 
+style="border-color: rgb(1, 108, 162);">
+<div id="text" 
+class="push-right didomi-notice-text" style="text-align: left;">
+<span>Con su acuerdo, nosotros y <a href="javascript:Didomi.preferences.show('vendors')">nuestros socios</a> usamos cookies o tecnologías similares para almacenar, acceder y procesar datos personales como su visita en este sitio web. Puede retirar su consentimiento u oponerse al procesamiento de datos basado en intereses legítimos en cualquier momento haciendo clic en "Configuración" o en nuestra Política de Cookies en este sitio web.</span>&nbsp;
+<div class="didomi-notice-data-processing-container">
+<div class="didomi-notice-data-processing-title">Nosotros y nuestros socios hacemos el siguiente tratamiento de datos:</div>
+  <div class="didomi-notice-data-processing-list">
+  <span>Almacenar o acceder a información en un dispositivo</span>
+  <span>, Anuncios y contenido personalizados, medición de anuncios y del contenido, información sobre el público y desarrollo de productos</span>
+  <span>, Compartir datos y perfiles no vinculados a su identidad</span>
+  <span>, Datos de localización geográfica precisa e identificación mediante las características de dispositivos</span>
+  </div>
+  </div>
+  </div>
+  <div id="buttons" 
+  class="multiple didomi-buttons"><button id="didomi-notice-learn-more-button" 
+  class="didomi-components-button didomi-button didomi-learn-more-button didomi-button-standard standard-button" aria-label="Configura tus consentimientos" style="display: block !important; color: rgb(68, 68, 68); background-color: rgb(238, 238, 238); border-radius: 0px; border-color: rgba(34, 34, 34, 0.2); border-width: 1px;"><span>Configuración</span></button><button id="didomi-notice-agree-button" 
+  class="didomi-components-button didomi-button didomi-dismiss-button didomi-components-button--color didomi-button-highlight highlight-button" aria-label="Aceptar nuestro procesamiento de datos y cerrar" style="display: block !important; color: rgb(255, 255, 255); background-color: rgb(1, 108, 162); border-radius: 0px; border-color: rgba(1, 108, 162, 0.3); border-width: 1px;">
+  <span>Aceptar</span>
+  </button>
+  </div>
+  </div>
+  </div>
